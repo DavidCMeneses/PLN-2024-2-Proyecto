beaa2d5e68a2ac5869cd39e6e752eeec0aa0fb68
FeliCruzV <felipecruzv10@gmail.com>
2024-11-20 12:05:28 -0500

    Refactor

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 DIRECTORIO=$(cd "$(dirname "$0")" && pwd)
 
 # Este comando inicia la API de la base de datos ICREA en una nueva terminal
-osascript -e "tell application \"Terminal\" to do script \"cd '$DIRECTORIO' && python_pln/bin/python3 Main.py\""
+osascript -e "tell application \"Terminal\" to do script \"cd '$DIRECTORIO' && python_pln/bin/python3 src/Main.py\""
--- src/Main.py
+++ src/Main.py
@@ -1,4 +1,6 @@
 from Dataset import Dataset
 
 # Cargar base de datos
+print('Running...')
 data = Dataset()
+print(data)
