ce3c3938b8a0d62ff9d8ab59018e33b051a244a7
FeliCruzV <felipecruzv10@gmail.com>
2024-11-20 12:11:46 -0500

    Typing

--- src/Dataset.py
+++ src/Dataset.py
@@ -1,6 +1,6 @@
 from datasets import load_dataset
 from datasets import Dataset
 
-def Dataset()->Dataset:
+def LoadDataset()->Dataset:
     ds = load_dataset("alfredplpl/artbench-pd-256x256")
     return ds
